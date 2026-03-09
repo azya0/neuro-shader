50abce0128079eb0c744c978b9d61ec49490a95c
azya0 <azyadr@yandex.ru>
2026-03-09 17:35:37 +0200

    feat(test): add new test module for debuging

--- source/test.py
+++ source/test.py
@@ -1,10 +1,13 @@
 from PIL import Image
 from torchinfo import summary
+import torch
 
 from model import MLP
 from noise_dataset import get_dataset, Constants, Noises, Data
 from godot.ImageTextureParser import parse
 from godot.ImageTexture3D import GodotTexture3DSampler
+from noise_dataset import Vector3, function, GET_DATA
+
 
 def image_test(filename: str):
     data = parse(filename)
@@ -36,6 +39,27 @@ def dataset_test() -> float:
     return get_dataset(data, 10000)
 
 
+def model_check(filepath: str):
+    model = MLP(sizes=(16, 16, 1), use_dropout=True)
+
+    model.load_state_dict(torch.load(filepath))
+    model.eval()
+
+    first, second = Vector3(0.1, 0.1, 0.1), Vector3(0.2, 0.5, 0.2)
+    print(f"Вектора: {first} -> {second}")
+
+    result = function(first, second, GET_DATA())
+
+    print(f"Результат: {result}")
+
+    input = torch.cat([first.to_tensor(), second.to_tensor()], dim=0).unsqueeze(0)
+
+    with torch.no_grad():
+        result: torch.Tensor = model(input)
+
+    print(f"Модель сказала: {result.item()}")
+
+
 def model_test():
     data = MLP((2, 32, 64, 1))
 
@@ -43,4 +67,5 @@ def model_test():
 
 
 if __name__ == "__main__":
-    dataset_test()
+    # image_test("../dataset/LargeImage.tres")
+    model_check("../model.pth")
